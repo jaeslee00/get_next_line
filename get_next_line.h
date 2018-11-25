f85292885b65df7848d711d6d78744cf76e85480
Jae seok LEE <jaelee@e3r5p9.42.fr>
2018-11-25 19:03:14 +0100

    get_next_line finished

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaelee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/14 09:16:13 by jaelee            #+#    #+#             */
+/*   Updated: 2018/11/23 22:17:01 by jaelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "./libft/libft.h"
+# define BUFF_SIZE 1
+# define FD_NBR 10000
+
+int				get_next_line(const int fd, char **line);
+
+#endif
